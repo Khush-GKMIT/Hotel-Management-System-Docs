994be45d963f247cc5f9b44b1c988b1914994e8e
Khush-GKMIT <khush@gkmit.co>
2025-11-14 12:45:02 +0530

    Updated files of technical docs

--- site/javascripts/mermaid-init.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  if (window.mermaid && !window.mermaid.initialized) {
-    window.mermaid.initialize({
-      startOnLoad: true,
-      securityLevel: 'loose'
-    });
-    window.mermaid.initialized = true;
-  }
-});
